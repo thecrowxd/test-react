9adb1df9100e625d3220f89bdb11c344f014b622
Nico <ntenes@gmail.com>
2021-04-16 13:25:23 +0200

    uploading db

---
+++ src/views/ProductsList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const ProductsList = () => {
+        return (
+            <Row>
+                <Col sm={9}>
+                    Product List
+                </Col>
+                <Col>
+                    Cart
+                </Col>
+            </Row>
+        )
+    
+}
+
+export default ProductsList
